f31d5b9a248ad150129c04e2da929ed022e530ec
Dhruvinhp <dhruvinhprajapati@gmail.com>
2021-09-22 17:47:27 +0530

    weather django app

---
+++ weatherApp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import City
+from .forms import CityForm
+import requests
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=541cc8c1887ab45994e03b1358cedac0"
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'id':city.id,
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+
+    return render(request, 'weather/index.html', context)
+
+def delete(request, id):
+    if request.method == 'POST':
+        print(request)
+        City.objects.filter(id=id).delete()
+    return redirect('/')
